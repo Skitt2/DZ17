ce0f0d252f9e5651ef25518867cff7daf5f987ad
Skitt2 <provaid@yandex.ru>
2022-04-03 11:21:38 +0300

    Добавьте файлы проекта.

---
+++ DZ17/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ17
+{
+    class BankAccount <T>
+    {
+        public T Num { get; set; }
+        public double Balance { get; set; }
+        public string Name { get; set; }
+
+        public BankAccount(T num, double balance, string name)
+        {
+            Num = num;
+            Name = name;
+            Balance = balance;
+        }
+
+        public string GetInfo()
+        {
+            return $"{Num} {Balance} {Name}";
+        }
+    }
+}
---
+++ DZ17/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числовой номер счета ");
+            int numInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите баланс счета ");
+            double balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО ");
+            string name = Console.ReadLine();
+            BankAccount<int> bankAccount1 = new BankAccount<int>(numInt, balance, name);
+            Console.WriteLine(bankAccount1.GetInfo());
+
+            Console.WriteLine("Введите строковый номер счета ");
+            string numString = Console.ReadLine();
+            Console.WriteLine("Введите баланс счета ");
+            balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО ");
+            name = Console.ReadLine();
+            BankAccount<string> bankAccount2 = new BankAccount<string>(numString, balance, name);
+            Console.WriteLine(bankAccount2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
